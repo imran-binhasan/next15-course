b522207afaa618a14b9c0ea4e49e5d00b2da068b
imran-binhasan <imranbinhasan.work@gmail.com>
2025-01-30 18:22:50 +0600

    initialize

---
+++ app/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function 
+() {
+  return (
+    <nav>
+        <ul className='flex justify-between'>
+            <li>Home</li>
+            <li>Services</li>
+            <li>About</li>
+        </ul>
+    </nav>
+  )
+}
